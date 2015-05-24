fbc2423c932ad43e25ac081319b9690e46f923a9
patrickang <ang.patrickwilliam@gmail.com>
2015-05-24 10:47:15 +0800

    Initial

---
+++ application/views/header.php
@@ -0,0 +1,6 @@
+<head>
+<link rel="stylesheet" href="<?php echo base_url();?>media/css/bootstrap.css">
+<link rel="stylesheet" href="<?php echo base_url();?>media/css/custom.css">
+<script src="<?php echo base_url();?>media/js/jquery-1.11.3.min.js"></script>
+<title><?php echo $title;?></title>
+</head>
---
+++ application/views/home.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="row">
+		<h1>Hello World!</h1>
+	</div>
+</div>
+
+</body>
+</html>
